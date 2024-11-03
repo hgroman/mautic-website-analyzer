dba97e604a56aff617b935267714e9af0aa6fa8b
Hank Groman <hank@lastapple.com>
2024-11-03 01:32:45 -0800

    Added settings file with production credentials

---
+++ src/config/settings.py
@@ -0,0 +1,6 @@
+MAUTIC_BASE_URL = 'https://mautic.henrygroman.com'
+MAUTIC_CLIENT_ID = '3_4s4qxg8dc8gw4w0c0k8s0c4wsw8w4440ckc0ggsgk4k4ko8cs'
+MAUTIC_CLIENT_SECRET = '576nj8xvmpk84wk0ks4gcw4k0k0kc8ow8g8gkc4k4kw4wo0ss'
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+LOG_LEVEL = 'INFO'
