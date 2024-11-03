0e3999a1dbeb478f84cebf557d0d3087b1d74c3c
Hank Groman <hank@lastapple.com>
2024-11-03 01:59:04 -0800

    Updated Mautic URL to lastapple.com

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,6 @@
-MAUTIC_BASE_URL = 'https://mautic.henrygroman.com'
-MAUTIC_CLIENT_ID = '3_4s4qxg8dc8gw4w0c0k8s0c4wsw8w4440ckc0ggsgk4k4ko8cs'
-MAUTIC_CLIENT_SECRET = '576nj8xvmpk84wk0ks4gcw4k0k0kc8ow8g8gkc4k4kw4wo0ss'
+MAUTIC_BASE_URL = 'https://mautic.lastapple.com'
+MAUTIC_CLIENT_ID = 'your_client_id'
+MAUTIC_CLIENT_SECRET = 'your_client_secret'
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 LOG_LEVEL = 'INFO'
